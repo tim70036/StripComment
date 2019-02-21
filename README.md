2602e247b4dee76d81726abc01c6d331dfc3fdaa
LazyShout <tim70036@gmail.com>
2019-02-21 17:39:13 +0800

    init

---
+++ README.md
@@ -0,0 +1,54 @@
+# StripComment
+
+A small convertor that strips comment for all files in a folder using strip-comments and Walk. 
+
+## Getting Started
+
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes.
+
+### Prerequisites
+
+After downloading, run
+
+```
+npm install
+```
+
+### Run
+
+Just run the convert.js and provide your target folder as argument
+
+```
+node convert.js $DIRECTORY
+```
+
+Example
+
+```
+node convert.js ../dir 
+```
+### Skip Directory
+
+Configure skipped.json to skip the directories you don't want to tamper with.
+
+```
+[
+    "node_modules", 
+    "assets", 
+    ".git"
+]
+```
+
+By default, 3 directories as shown above will be ignored while stripping comment.
+
+
+## Built With
+
+* [strip-comment](https://www.npmjs.com/package/strip-comments) - Strip comments from code
+* [Walk](https://maven.apache.org/) - Walk file
+
+
+
+
+
+
---
+++ convert.js
@@ -0,0 +1,66 @@
+const
+    walk = require('walk'),
+    fs = require('fs'),
+    strip = require('strip-comments');
+
+// Init
+const 
+    dir = process.argv[2],
+    skippedDirs = require('./skipped.json');
+
+if(!dir) {
+    console.log('Please provide dir name in second args');
+    process.exit(0);
+}
+if(!skippedDirs){
+    console.log('Please provide skipped.json in current directory');
+    process.exit(0);
+}
+
+console.log(`Walking in directory '${dir}'`)
+run(dir, skippedDirs);
+
+// Main func
+function run(dir, skippedDirs){
+
+    // Init
+    let options = {
+        followLinks: false
+        // directories with these keys will be skipped
+      , filters: skippedDirs
+    };
+
+    let walker = walk.walk(dir, options);
+
+    // Set callback
+    walker.on("directories", function (root, dirStatsArray, next) {
+        // dirStatsArray is an array of `stat` objects with the additional attributes
+        // * type
+        // * error
+        // * name
+        next();
+    });
+
+    walker.on("file", function (root, fileStats, next) {
+
+        console.log(`${root}/${fileStats.name}`);
+        let content = fs.readFileSync(`${root}/${fileStats.name}`, 'utf-8');
+        //console.log(content);
+
+        let converted = strip(content);
+        //console.log(converted);
+        
+        fs.writeFileSync(`${root}/${fileStats.name}`, converted)
+
+        next();
+    });
+
+    walker.on("errors", function (root, nodeStatsArray, next) {
+        next();
+    });
+
+    walker.on("end", function () {
+        console.log("Walking is done");
+    });
+
+}
